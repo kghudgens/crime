c092752ab8de040975a8fee2e4f3502e73c53f60
Kevin Hudgens <hudgens1073@gmail.com>
2021-07-06 20:34:02 +0900

    imported posts to root component

--- crime-ward/src/App.js
+++ crime-ward/src/App.js
@@ -1,27 +1,10 @@
 import "./App.css";
-
-const Navbar = () => {
-  return (
-    <div className="container">
-      <h1 className="title">Crime Ward</h1>
-      <ul className="menu navigation-bar">
-        <li>Home</li>
-        <li>Log In</li>
-        <li>Register</li>
-      </ul>
-    </div>
-  );
-};
-
-const Main = () => {
-  return <h1>Posts</h1>;
-};
+import Posts from "./Posts";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Main />
+    <div>
+      <Posts />;
     </div>
   );
 }
