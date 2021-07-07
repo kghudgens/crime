5eaac8bc2806683e094ac343ba2de09eb61ad0cf
Kevin Hudgens <hudgens1073@gmail.com>
2021-07-07 20:13:51 +0900

    Created Posts component with placeholders

---
+++ crime-ward/src/components/Content/Posts.js
@@ -0,0 +1,15 @@
+import "./Posts.css";
+
+const Posts = (props) => {
+  return (
+    <div>
+      <h2>Title</h2>
+      <h3>Author</h3>
+      <h3>Date</h3>
+      <h3>Location</h3>
+      <p>Content</p>
+    </div>
+  );
+};
+
+export default Posts;
