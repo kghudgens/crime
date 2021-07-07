40d5874523941942eccc3c97a97ff621740b98e3
Kevin Hudgens <hudgens1073@gmail.com>
2021-07-07 20:13:22 +0900

    Created Navbar component

---
+++ crime-ward/src/components/UI/Navbar.js
@@ -0,0 +1,17 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Crime Ward</li>
+        <li>About</li>
+        <li>Post</li>
+        <li>Sign In</li>
+        <li>Log In </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
