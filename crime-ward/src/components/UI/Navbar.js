16edb05e7ae5c473c72621962a6f0993518787ac
Kevin Hudgens <hudgens1073@gmail.com>
2021-07-07 20:36:59 +0900

    added bootstrap css

--- crime-ward/src/components/UI/Navbar.js
+++ crime-ward/src/components/UI/Navbar.js
@@ -2,15 +2,40 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      <ul>
-        <li>Crime Ward</li>
-        <li>About</li>
-        <li>Post</li>
-        <li>Sign In</li>
-        <li>Log In </li>
-      </ul>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <a class="navbar-brand" href="#">
+        Crime Ward
+      </a>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              About
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Posts
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Sign Up
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Log In
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
