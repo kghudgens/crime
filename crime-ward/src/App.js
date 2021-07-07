b74f4a4ec47d78698793d74a4a89b84820ade5f3
Kevin Hudgens <hudgens1073@gmail.com>
2021-07-07 20:14:28 +0900

    imported the navbar

--- crime-ward/src/App.js
+++ crime-ward/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-import Posts from "./components/Posts";
+import Posts from "./components/Content/Posts";
+import Navbar from "./components/UI/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />;
       <Posts />;
     </div>
   );
