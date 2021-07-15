609b2d9c1964b889880b862ab45541e219bef960
Kevin Hudgens <hudgens1073@gmail.com>
2021-07-15 22:42:09 +0900

    added test data to display

--- crime-ward/src/App.js
+++ crime-ward/src/App.js
@@ -3,6 +3,23 @@ import Posts from "./components/Content/Posts";
 import Navbar from "./components/UI/Navbar";
 
 function App() {
+  const postList = [
+    {
+      title: "Murder",
+      author: "kevinhudgens",
+      date: new Date(2021, 7, 11),
+      location: "South Carolina",
+      content: "There was a killing in south carolina ",
+    },
+    {
+      title: "Robbery",
+      author: "Momo",
+      date: new Date().toISOString(),
+      location: "New York",
+      content: "There was a Robbery in new york",
+    },
+  ];
+
   return (
     <div>
       <Navbar />;
