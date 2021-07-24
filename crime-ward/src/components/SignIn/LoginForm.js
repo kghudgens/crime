40a0f80b0b7844b2b927ea1fd18b79ce939d7279
Kevin Hudgens <hudgens1073@gmail.com>
2021-07-24 11:27:08 +0900

    Created the login form

--- crime-ward/src/components/SignIn/LoginForm.js
+++ crime-ward/src/components/SignIn/LoginForm.js
@@ -4,7 +4,19 @@ import React from "react";
 const LoginForm = () => {
   return (
     <div>
-      <form></form>
+      <form>
+        <div>
+          <label>Username</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="text" />
+        </div>
+        <div>
+          <button>Sign In</button>
+        </div>
+      </form>
     </div>
   );
 };
