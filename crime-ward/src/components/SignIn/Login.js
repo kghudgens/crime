eb48a5d925bff9b3647e1df7d1c451b88c3abf2e
Kevin Hudgens <hudgens1073@gmail.com>
2021-07-31 20:16:36 +0900

    created modal

--- crime-ward/src/components/SignIn/Login.js
+++ crime-ward/src/components/SignIn/Login.js
@@ -4,8 +4,18 @@ import LoginForm from "./LoginForm";
 
 const Login = () => {
   return (
-    <div>
-      <LoginForm />
+    <div className="modal">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h4 className="modal-title"> Please Log In</h4>
+        </div>
+        <div className="modal-body">
+          <LoginForm />
+        </div>
+        <div>
+          <button className="button">Close</button>
+        </div>
+      </div>
     </div>
   );
 };
